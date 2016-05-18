73a1c68788b20c8678ddce84c9c4e57e934e9542
eta4ever <arlorlo@ya.ru>
2016-05-18 14:15:27 +0300

    pid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -162,9 +162,9 @@
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+    #define  DEFAULT_Kp 27.86
+    #define  DEFAULT_Ki 5.49
+    #define  DEFAULT_Kd 35.38
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
